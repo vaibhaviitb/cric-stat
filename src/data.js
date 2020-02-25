14ba7432489050ad4c39b491fa1a777138690ce6
Vaibhav Kumar <vaibhav.kumar@pwc.com>
2020-02-26 03:12:28 +0530

    runnign code commit

---
+++ src/data.js
@@ -0,0 +1,88 @@
+export const stats=[
+    {
+        name: 'Ravichandran Ashwin',
+        id:1,
+        dob: '17-09-1986',
+        country: 'India',
+        role: 'Bowling Akllrounder',
+        bowlingStyle: 'right-arm offbreak', 
+        battingStyle: 'right-hand-bat',
+        odi:{
+            matches: 111,
+            fielding:{
+                catches:30,
+                stumpings:0
+            },
+            batting: {
+                innings: 61,
+                notOut: 19,
+                runs: 675,
+                highestScore: 65,
+                average: 16.07,
+                bf: 776,
+                strikeRate: 86.98,
+                '100s': 0,
+                '50s': 1,
+                '4s':59,
+                '6s': 6
+            },
+            bowling: {
+                innings: 109,
+                bowls: 6021,
+                runs: 4937,
+                wickets: 150,
+                bestBowlingIndex: '4/25',
+                average: 32.91,
+                economy: 4.91,
+                strikeRate:40.1,
+                '4W': 1,
+                '5W': 0
+
+            } 
+        }
+    },
+    {
+        name: 'David Andrew Warner',
+        id:2,
+        dob: '27-10-1986',
+        country: 'Australia',
+        nickName: 'Lloyd',
+        role: 'Opening Batsman',
+        bowlingStyle: 'leg-break', 
+        battingStyle: 'left-hand bat',
+        heightInMetres: 1.7,
+        odi:{
+            matches: 119,
+            fielding:{
+                catches:54,
+                stumpings:0
+            },
+            batting: {
+                innings: 117,
+                notOut: 6,
+                runs: 5136,
+                highestScore: 179,
+                average: 46.27,
+                bf: 5353,
+                strikeRate: 95.94,
+                '100s': 18,
+                '50s': 20,
+                '4s':539,
+                '6s': 81
+            },
+            bowling: {
+                innings: 1,
+                bowls: 6,
+                runs: 8,
+                wickets: 0,
+                bestBowlingIndex: 'NA',
+                average: 'NA',
+                economy: '8',
+                strikeRate:'NA',
+                '4W': 0,
+                '5W': 0
+
+            } 
+        }
+    }
+];
